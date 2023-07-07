793128bbc535138a1faa8d01a0268485934825ec
f.afshani <fatemeh.afshani78@gmail.com>
2023-07-07 11:39:00 +0330

    feat: add user module

---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,42 @@
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UsersRepository } from './users.repository';
+import { CreateUserRequest } from './dto/create-user.request';
+import { User } from './schemas/user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async createUser(request: CreateUserRequest) {
+    let user: User;
+    user = await this.usersRepository.findOne({
+      email: request.email,
+    });
+    if (user) {
+      throw new UnprocessableEntityException('Email already exists.');
+    }
+    user = await this.usersRepository.create({
+      ...request,
+      password: await bcrypt.hash(request.password, 10),
+    });
+    return user;
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+    return user;
+  }
+
+  async getUser(getUserArgs: Partial<User>) {
+    return this.usersRepository.findOne(getUserArgs);
+  }
+}
